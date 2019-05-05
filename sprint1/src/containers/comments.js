66fc10e5db1cb06b510e6415bc63da7883538695
Michael <michaelkimsing@gmail.com>
2019-05-04 19:53:05 -0700

    Basic comment validation and error message

--- sprint1/src/containers/comments.js
+++ sprint1/src/containers/comments.js
@@ -33,28 +33,72 @@ class Comments extends Component {
   }
 }
 
-function CommentSubmissionBlock(props) {
-  let { numComments, avatar, submitComment } = props;
-  let submitHandler = (event) => {
+
+class CommentSubmissionBlock extends Component {
+
+  state = {
+    submissionMsg: "Comment text is required!",
+    submissionMsgClasses: ['submissionMsg--hidden']
+  }
+  submitHandler = (event) => {
+    const commentField = event.target.commentText;
     event.preventDefault();
-    submitComment(event.target.commentText.value);
-    event.target.commentText.value = "";
+    if (!this.isEmptySubmission(commentField)) {
+      this.props.submitComment(commentField.value);
+      commentField.value = "";
+      this.handleErrorStyling(false);
+    }
+    else {
+      this.handleErrorStyling(true);
+    }
+
   }
 
-  return (
-    <div className='submission'>
-      <h2 className='submission__commentCount'>{numComments} Comments</h2>
-      <h5 className='submission__title'> JOIN THE CONVERSATION</h5>
-      <div className='submission__container'>
-        <img className="avatar submission__avatar" src={avatar} alt="Avatar" />
-        <form onSubmit={submitHandler}>
-          <textarea name='commentText' placeholder='Add a comment' />
-          <button type='submit'> COMMENT </button>
-        </form>
-      </div>
-      <hr />
-    </div>
-  );
+  /**
+ * Check if fields are empty/default value and call functions to apply/remove error styling
+ */
+  isEmptySubmission = (commentField) => {
+    if (
+      commentField.value === commentField.defaultValue ||
+      !commentField.value.trim() //Checks for all whitespace (newline, space, tab, etc)
+    ) {
+      return true;
+    }
+  }
+
+  handleErrorStyling = (applyStyling) => {
+    let newClasses;
+    if (applyStyling) {
+      newClasses = ['submissionMsg--errorGroup'];
+    }
+    else {
+      newClasses = ['submissionMsg--hidden'];
+    }
+
+    this.setState({
+      submissionMsgClasses: newClasses
+    })
+  }
+  render() {
+    let { numComments, avatar } = this.props;
+    return (
+      <div className='submission'>
+        <h2 className='submission__commentCount'>{numComments} Comments</h2>
+        <h5 className='submission__title'> JOIN THE CONVERSATION</h5>
+        <div className='submission__container'>
+          <img className="avatar submission__avatar" src={avatar} alt="Avatar" />
+          <form onSubmit={this.submitHandler}>
+            <div className='inputGroup'>
+              <textarea name='commentText' placeholder='Add a comment' />
+              <h3 className={String(...this.state.submissionMsgClasses)}> {this.state.submissionMsg}</h3>
+            </div>
+            <button type='submit'> COMMENT </button>
+          </form>
+        </div>
+        <hr />
+      </div >
+    );
+  }
 }
 
 
