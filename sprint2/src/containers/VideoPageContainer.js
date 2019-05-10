d16fc441daf037fb2b9574d2fa4c7d86d276f315
Michael <michaelkimsing@gmail.com>
2019-05-09 22:48:24 -0700

    Minor styling tweak on delete button and simplistic confirm before deleting comments

--- sprint2/src/containers/VideoPageContainer.js
+++ sprint2/src/containers/VideoPageContainer.js
@@ -108,7 +108,10 @@ class MainVideoContainer extends Component {
   };
 
   deleteComment = commentId => {
-    //TODO add confirmation before deleting
+    //TODO add better confirmation before deleting
+    if (!window.confirm('Are you sure you want to delete the comment?')) {
+      return
+    }
     axios
       .delete(
         `${apiInfo.API_URL}/videos/${
